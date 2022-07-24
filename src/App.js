15c77bc4088ecc51def30eae6e6901b5211b795b
sohambhowmick <sohamslc@gmail.com>
2022-07-24 16:51:52 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed on tyhe feature branch
+          I was changed on feature branch
           {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         <a
